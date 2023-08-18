39052b5f071b196f2d2a6ab25d5442b955a5929b
SamZakharov <semen.zaharovv@gmail.com>
2023-08-18 18:47:39 +0300

    Completed task 3.Математичнi операцiї

---
+++ homework.js
@@ -0,0 +1,13 @@
+const firstNumber = Number(prompt('Enter first number'));
+const secondNumber = Number(prompt('Enter second number'));
+const additionResult = Number(firstNumber + secondNumber);
+const subtractionResult = Number(firstNumber - secondNumber);
+const multiplicationResult = Number(firstNumber * secondNumber);
+const divisionResult = Number(firstNumber / secondNumber);
+
+
+
+alert(firstNumber + ' + ' + secondNumber + ' = ' + additionResult);
+alert(firstNumber + ' - ' + secondNumber + ' = ' + subtractionResult);
+alert(firstNumber + ' * ' + secondNumber + ' = ' + multiplicationResult);
+alert(firstNumber + ' / ' + secondNumber + ' = ' + divisionResult);
