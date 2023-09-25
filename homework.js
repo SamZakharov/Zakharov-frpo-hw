11f84c14cb406088fb1956b8d465e9d4600bffbc
SamZakharov <semen.zaharovv@gmail.com>
2023-09-25 11:53:05 +0300

    24. числа та рядки.

---
+++ homework.js
@@ -0,0 +1,14 @@
+
+// 1.
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+    arr.push(prompt('Введите слово:'))
+}
+
+alert(arr.join(''));
+
+// 2.
+// let numb = 12345;
+//
+// console.log(String(numb).split('').join(' '));
