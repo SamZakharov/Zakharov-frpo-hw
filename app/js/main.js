fab56f378e1f39e62ac55d85e57e66b0eeac1d18
SamZakharov <semen.zaharovv@gmail.com>
2023-10-30 20:00:20 +0200

    38. Gulp

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+console.log(add(2,4));
+console.log(divide(8,4));
+console.log(multiply(8,4));
